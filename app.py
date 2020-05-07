c805fc1f9c12511f8fb129fa3f277e76df10cd3d
Equihua <luis.herrerae@outlook.com>
2020-05-07 00:08:35 -0500

    Find all and by id services

--- app.py
+++ app.py
@@ -1,6 +1,40 @@
-from flask import Flask
+from flask import Flask, jsonify
+from employees import employees
+
 
 app = Flask(__name__)
+
+
+@app.route('/test')
+def test():
+    return 'working!'
+
+
+@app.route('/employees')
+def findEmployees():
+    return jsonify({
+        "employees": employees
+    })
+
+
+@app.route('/employees/<employeeId>')
+def findEmployeeById(employeeId):
+
+    employee_found = unique_employee(employeeId)
+    return jsonify({
+        "employee": employee_found
+    })
+
+
+def unique_employee(employee_id):
+
+    for employee in employees:
+        print(employee['id'])
+        if employee['id'] == employee_id:
+            return employee
+
+    raise Exception('No employee found with that id')
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=9000)
